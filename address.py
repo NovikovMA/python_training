b2a0620f7f76f08a440fa4f8108f1b6b9f7e9797
NovikovMA <malnovikov@yandex.ru>
2015-05-20 19:43:10 +0400

    Два новых теста добавления контакта
    1. С помощью Selenium Builder записан тестовый сценарий добавления
    нового контакта .
    2. Записанный сценарий добавлен к проекту.
    3. Для повышения читаемости кода, выделены вспомогательные методы.
    4. Создан вспомогательный класс, хранящий параметры контакта.

---
+++ address.py
@@ -0,0 +1,30 @@
+__author__ = 'M.Novikov'
+
+
+class Address:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, tel_home, tel_mobile, tel_work, tel_fax, web_email, web_email2, web_email3, web_homepage, birthday_day, birthday_month, birthday_year, anniversary_day, anniversary_month, anniversary_year, sec_address, home, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.tel_home = tel_home
+        self.tel_mobile = tel_mobile
+        self.tel_work = tel_work
+        self.tel_fax = tel_fax
+        self.web_email = web_email
+        self.web_email2 = web_email2
+        self.web_email3 = web_email3
+        self.web_homepage = web_homepage
+        self.birthday_day = birthday_day
+        self.birthday_month = birthday_month
+        self.birthday_year = birthday_year
+        self.anniversary_day = anniversary_day
+        self.anniversary_month = anniversary_month
+        self.anniversary_year = anniversary_year
+        self.sec_address = sec_address
+        self.home = home
+        self.notes = notes
